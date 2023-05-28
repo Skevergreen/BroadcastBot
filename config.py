a2814b121fc15457435b5a23255e3fe21c7ccfa1
Skevergreen <116793473+Skevergreen@users.noreply.github.com>
2023-05-28 21:33:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
-AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
-DB_URL = os.environ.get("DB_URL", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5784419367:AAHahvpKTqaPvws-f7ZT7wNGDJrOq8MYdLY")
+API_ID = int(os.environ.get("API_ID", "23937617"))
+API_HASH = os.environ.get("API_HASH", "cdc408c8bf00c9c2219fee9cd585bbda")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001637312289"))
+AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1412758888").split())
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://Anime560:Anime560@cluster0.aqk0z3p.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DB_NAME", "BroadcastBot")
 BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
